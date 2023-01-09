95f14a4ffdecb17548682d3bba0c413473fe2b2f
Stephen Strawbridge <stephen.strawbridge92@gmail.com>
2023-01-09 11:50:33 -0800

    Added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. The current temperature is ' + body.current.temperature + ' and it feels like ' + body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] + '. The current temperature is ' + body.current.temperature + ' and it feels like ' + body.current.feelslike
+                        + ". The humidity is " + body.current.humidity + "%.")
         }
     })
 }
